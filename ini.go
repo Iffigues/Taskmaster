5473929537401cadbd05c60884adf53aa3fcc6de
root <root@vps683807.ovh.net>
2019-05-07 17:21:37 +0200

    lol

---
+++ ini.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"github.com/go-ini/ini"
+	"log"
+)
+
+const (
+	bdd = "bdd"
+	htp = "htp"
+)
+
+var (
+	cfg, cfgErr = ini.Load("ini.ini")
+)
+
+func init() {
+	get()
+}
+
+func get() {
+	ar := cfg.SectionStrings()
+	for _, ok := range ar {
+		if (ok != "DEFAULT") {
+			fmt.Println(ok);
+		}
+	}
+}
+
+func getKey(section, key string) (inu string) {
+	ar, err := cfg.Section(section).GetKey(key)
+	if err != nil {
+		log.Panic(err)
+	}
+	return ar.String()
+}
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import ()
+
+var ()
+
+func init() {
+}
+
+func main() {
+
+}
