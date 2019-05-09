5909aa59262ab7c4b2ed784e228a72639975bc2e
root <root@vps683807.ovh.net>
2019-05-09 19:14:15 +0200

    lolgp

--- opt.go
+++ opt.go
@@ -1,1 +1,23 @@
 package main
+
+var (
+	comopt = map[string]func(){
+		"start":    nil,
+		"stop":     nil,
+		"reread":   nil,
+		"quit":     nil,
+		"fg":       nil,
+		"clear":    nil,
+		"maintail": nil,
+		"reload":   nil,
+		"restart":  nil,
+		"shutdown": nil,
+		"signal":   nil,
+		"status":   nil,
+		"tail":     nil,
+		"update":   nil,
+		"version":  nil,
+	}
+	comserv   = map[string]func(){}
+	comcleint = map[string]func(){}
+)
