62135ea0b7ef9c379141fbdd22cf19a93886206d
Boris DENOYELLE <bordenoy@e2r7p5.42.fr>
2019-12-14 15:16:32 +0100

    push

--- Taskmasterd/finish.go
+++ Taskmasterd/finish.go
@@ -5,5 +5,5 @@ import (
 )
 
 func finish(fifi float64, cc, exe int) (a, b bool, g time.Time, d float64, y bool, i int) {
-	return false, true, time.Now(), fifi, fifi >= float64(time.Second*time.Duration(cc)), exe + 1
+	return false, true, time.Now(), fifi, int(fifi) >= int(cc), exe + 1
 }
