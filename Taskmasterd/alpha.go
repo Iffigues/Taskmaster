c0a8f64e21b36f5db51b7931060231b75c835757
Boris DENOYELLE <bordenoy@e2r3p10.42.fr>
2019-12-15 18:37:39 +0100

    push

--- Taskmasterd/alpha.go
+++ Taskmasterd/alpha.go
@@ -22,9 +22,28 @@ func padding() (i int) {
 	return i + 1
 }
 
+func getPadding(a ...string) (i int) {
+	for _, val := range a {
+		b := len(val)
+		if b > i {
+			b = i
+		}
+	}
+	return i + 1
+}
+
+func getAlpha(a ...string) (keys []string) {
+	keys = make([]string, 0, len(a))
+	for _, k := range a {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+	return
+}
+
 func alpha() (keys []string) {
 	keys = make([]string, 0, len(jobs))
-	for k := range jobs {
+	for k, _ := range jobs {
 		keys = append(keys, k)
 	}
 	sort.Strings(keys)
